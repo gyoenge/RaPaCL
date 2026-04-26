e1eda58d6ebd080fd1e20f4a27f0fe9f65dfde61
gyoenge <gyoeng94@gmail.com>
2026-04-26 23:49:11 +0900

    refactor: remove previous rapacl directory

--- src/rapacl/models/heads/__init__.py
+++
@@ -1,9 +0,0 @@
-from .projection import ProjectionHead
-from .classifier import LinearClassifier
-from .batch_discriminator import AdversarialDiscriminator
-
-__all__ = [
-    "ProjectionHead",
-    "LinearClassifier",
-    "AdversarialDiscriminator",
-]
